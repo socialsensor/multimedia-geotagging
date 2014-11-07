771eb59034f479b8af92606bfc61f92d57f2d4e0
gkordo <georgekordopatis@hotmail.com>
2014-11-07 12:59:22 +0200

    Add comments to TagInfo.java

--- src/main/java/gr/iti/mklab/data/TagInfo.java
+++ src/main/java/gr/iti/mklab/data/TagInfo.java
@@ -8,7 +8,14 @@ import java.util.Map;
 import java.util.Map.*;
 import java.util.Set;
 
-
+/**
+ * The class that contains all the information for a specific tag, such as the actual tag, 
+ * the map of tag-cell probabilitiesrelative to the different users that used it,
+ * the total count of the different users that used it in all cells 
+ * and the total different users that used it in all dataset.
+ * @author gkordo
+ * 
+ */
 public class TagInfo extends HashMap<String, Double>{
 
 	private static final long serialVersionUID = 1L;
@@ -17,6 +24,10 @@ public class TagInfo extends HashMap<String, Double>{
 	private String tag;
 	private int timesFound;
 
+	/**
+	 * Contractor initialize the needed variables
+	 * @param tag
+	 */
 	public TagInfo(String tag){
 		this.mapOfTagsUserPerCell = new HashMap<String,Set<String>>();
 		this.totalUsersUseTag = new HashSet<String>();
@@ -24,17 +35,22 @@ public class TagInfo extends HashMap<String, Double>{
 		this.timesFound = 0;
 	}
 
+	/**
+	 * Update the tag-cell probability map, when a new entry arrives.
+	 * @param cellID : the cell that the tag was found
+	 * @param userID : the user that used the tag
+	 */
 	public void updateMap(String cellID, String userID) {
 		if(!totalUsersUseTag.contains(userID)){totalUsersUseTag.add(userID);}
 
-		if(mapOfTagsUserPerCell.containsKey(cellID)){
-			if(!mapOfTagsUserPerCell.get(cellID).contains(userID)){
+		if(mapOfTagsUserPerCell.containsKey(cellID)){ 
+			if(!mapOfTagsUserPerCell.get(cellID).contains(userID)){ // update the existing records, if the user hasn't already found in the exact cell
 				timesFound++;
 				Set<String> tmpUserInCell = mapOfTagsUserPerCell.get(cellID);
 				tmpUserInCell.add(userID);
 				mapOfTagsUserPerCell.put(cellID, tmpUserInCell);
 			}
-		}else{
+		}else{ // create new records for the new entries
 			timesFound++;
 			Set<String> tmpUserInCell = new HashSet<String>();
 			tmpUserInCell.add(userID);
@@ -45,7 +61,12 @@ public class TagInfo extends HashMap<String, Double>{
 	public int getTotalUser(){
 		return totalUsersUseTag.size();
 	}
-
+	
+	/**
+	 * Calculation of the tag-cell probabilities.
+	 * @param sumToOne : indicates that either total count or just the total of different users will be used for the calculation of the tag-cell probabilities
+	 * @return a string line with all the tag-cell probabilities sorted
+	 */
 	public String calculateCellProb(boolean sumToOne){
 		Map<String,Double> cellsProbs = new HashMap<String,Double>();
 		String cellsProbsSortedStr = "";
@@ -84,11 +105,5 @@ public class TagInfo extends HashMap<String, Double>{
 		}
 
 		return out;
-	}
-	
-	public double computeTagProb(int totalUsers){
-		
-		return (double)totalUsersUseTag.size()/(double)totalUsers;
-		
 	}
 }
