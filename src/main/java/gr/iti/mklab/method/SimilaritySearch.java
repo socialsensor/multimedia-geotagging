a9b0abbb18a05fd041b009869b60495c0bfca742
gkordo <georgekordopatis@hotmail.com>
2014-11-11 11:23:00 +0200

    Add comments to SimilaritySearch.java

--- src/main/java/gr/iti/mklab/method/SimilaritySearch.java
+++ src/main/java/gr/iti/mklab/method/SimilaritySearch.java
@@ -1,4 +1,5 @@
 package gr.iti.mklab.method;
+
 import gr.iti.mklab.data.ImageMetadata;
 import gr.iti.mklab.util.MyHashMap;
 
@@ -9,13 +10,17 @@ import java.util.Map;
 import java.util.Set;
 import java.util.HashSet;
 
-
+/**
+ * Class that implements similarity search for a query image in a list of images calculating the similarity between the corresponding tags
+ * @author gkordo
+ *
+ */
 public class SimilaritySearch extends AbstractSimilaritySearch{
-
+	
+	// Constractor initializes the required values of the method
 	public SimilaritySearch(int k, int a, double threshold){
 		super(k,a,threshold);
 	}
-
 
 	public Double[] computeSimilarity(String imageTags, 
 			List<ImageMetadata> images){
@@ -24,6 +29,7 @@ public class SimilaritySearch extends AbstractSimilaritySearch{
 
 		String [] testImageTags = imageTags.split(",");
 
+		// calculate similarity for every image of the given set
 		for (int i=0; i<images.size(); i++){
 
 			double counter = 0.0;
@@ -49,10 +55,12 @@ public class SimilaritySearch extends AbstractSimilaritySearch{
 
 		MyHashMap.sortByValues(mapSim);
 
+		// check if the silimar images are less than k
 		if(mapSim.size()<k){
 			size = mapSim.size();
 		}
 
+		// check how many images are above the provided threshold
 		for (int i=0; i<size; i++){
 			if (mapSim.get(i)>threshold){
 				t++;
